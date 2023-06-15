d7638b3fac969ca067187463797747d9d0fbb78a
Lucas Rodiadis <lucas2@live.com.au>
2023-06-16 08:22:40 +1000

    Rewrote loop

--- src/platform/win32_platform.cpp
+++ src/platform/win32_platform.cpp
@@ -3,46 +3,26 @@
 #include "WindowsInputPoller.h"
 #include <assert.h>
 
+#include <chrono>
+#include <thread>
+
+#define TICKS_PER_SECOND 1
+
 typedef unsigned __int64 QWORD;
 
 static bool running = true;
-UINT MSG_GETRIBUFFER = RegisterWindowMessage("MSG_GETRIBUFFER");
+UINT MSG_GETRIBUFFER = RegisterWindowMessage(L"MSG_GETRIBUFFER");
 
 // Callback function occurs whenever windows does
 // literally anything concerning the window
 // eg resizing the window, minimising it, maximising it etc
 LRESULT CALLBACK platform_windows_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+HWND setupWindow(HINSTANCE hInstance);
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-    LPCSTR MAIN_WC_NAME = "Sample Window Class";
-
-    WNDCLASS MAIN_WC = {};
-    MAIN_WC.lpfnWndProc = platform_windows_callback;
-    MAIN_WC.hInstance = hInstance;
-    MAIN_WC.lpszClassName = MAIN_WC_NAME;
-
-    // Register the main window class
-    if (!RegisterClass(&MAIN_WC))
-    {
-        MessageBoxA(0, "Failed to register main window class.", "Error", MB_ICONEXCLAMATION | MB_OK);
-        return -1;
-    }
-
-    HWND hwnd = CreateWindowEx(
-        0,                   // Optional window styles.
-        MAIN_WC_NAME,        // Window class
-        "The funny pog",     // Window text
-        WS_OVERLAPPEDWINDOW, // Window style
-
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-        NULL,      // Parent window
-        NULL,      // Menu
-        hInstance, // Instance handle
-        NULL       // Additional application data
-    );
+    HWND hwnd = setupWindow(hInstance);
 
     // Return -1 if window does not initialise, failed to start
     if (hwnd == NULL)
@@ -54,11 +34,14 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     ShowWindow(hwnd, SW_SHOW);
 
+    unsigned MS_BETWEEN_UPDATES = 1000 / TICKS_PER_SECOND;
+
     while (running)
     {
-        PostMessage(hwnd, MSG_GETRIBUFFER, 0, 0);
+        // PostMessage(hwnd, MSG_GETRIBUFFER, 0, 0);
         // std::cout << "Value of a: " << (isDown ? "o" : "x") << std::endl;
-    }
+        std::this_thread::sleep_for(std::chrono::milliseconds(MS_BETWEEN_UPDATES));
+    };
 
     return 0;
 }
@@ -76,6 +59,40 @@ LRESULT CALLBACK platform_windows_callback(HWND hwnd, UINT uMsg, WPARAM wParam, 
         break;
     }
     return DefWindowProcA(hwnd, uMsg, wParam, lParam);
+}
+
+HWND setupWindow(HINSTANCE hInstance)
+{
+	LPWSTR MAIN_WC_NAME = L"Sample Window Class";
+
+    WNDCLASS MAIN_WC = {};
+    MAIN_WC.lpfnWndProc = platform_windows_callback;
+    MAIN_WC.hInstance = hInstance;
+    MAIN_WC.lpszClassName = MAIN_WC_NAME;
+
+    // Register the main window class
+    if (!RegisterClass(&MAIN_WC))
+    {
+        MessageBoxA(0, "Failed to register main window class.", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return NULL;
+    }
+
+    HWND hwnd = CreateWindowEx(
+        0,                   // Optional window styles.
+        MAIN_WC_NAME,        // Window class
+        L"The funny pog",     // Window text
+        WS_OVERLAPPEDWINDOW, // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,      // Parent window
+        NULL,      // Menu
+        hInstance, // Instance handle
+        NULL       // Additional application data
+    );
+
+    return hwnd;
 }
 
 void doPoll()
