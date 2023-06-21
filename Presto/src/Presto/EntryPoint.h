eeda16c73c3ab36dd22f92f8d331b4191a9ef619
Lucas Rodiadis <lucas2@live.com.au>
2023-06-21 21:40:39 +1000

    added logger

--- Presto/src/Presto/EntryPoint.h
+++ Presto/src/Presto/EntryPoint.h
@@ -6,6 +6,10 @@
 extern Presto::Application* Presto::CreateApplication();
 
 int main(int argc, char** argv) {
+    Presto::Log::Init();
+    PR_CORE_WARN("Initialised core logger.");
+    PR_INFO("Initialised client logger.");
+
     auto app = Presto::CreateApplication();
     app->Run();
     delete app;
