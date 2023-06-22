e7d328e87702dc53dbf46172ef42f13d3b07895a
Lucas Rodiadis <lucas2@live.com.au>
2023-06-22 13:13:37 +1000

    Builds and runs

---
+++ premake5.lua
@@ -0,0 +1,95 @@
+workspace "Presto"
+	architecture "x64"
+
+	configurations{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Presto"
+	location "Presto"
+	kind "SharedLib" -- DLL
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"  -- WINDOWS ONLY
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "10.0.22621.0"  -- WINDOWS SDK VERSION
+
+		defines {
+			"PR_PLATFORM_WINDOWS", "PR_BUILD_DLL"
+		}
+
+		postbuildcommands{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Duality")
+		}
+
+	filter "configurations:Debug"
+		defines "PR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PR_DIST"
+		optimize "On"
+
+project "Duality"
+	location "Duality"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+	}
+
+	includedirs {
+		"Presto/vendor/spdlog/include",
+		"Presto/src"
+	}
+
+	links {
+		"Presto"
+	}
+
+	filter "system:windows"  -- WINDOWS ONLY
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "10.0.22621.0"  -- WINDOWS SDK VERSION
+
+		defines {
+			"PR_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "PR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "PR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "PR_DIST"
+		optimize "On"
