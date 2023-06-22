b100c241d373ffda12cb8b3b394f85441bad7531
Lucas Rodiadis <lucas2@live.com.au>
2023-06-22 16:35:01 +1000

    Input almost working, no target architecture

--- Duality/src/DualityApp.cpp
+++ Duality/src/DualityApp.cpp
@@ -4,6 +4,13 @@ class DualityApp : public Presto::Application {
    public:
     DualityApp(){}
     ~DualityApp(){}
+
+    void Run() {
+        for (auto i = 0; i < 10; i++) {
+            Presto::Thumbstick vals = Presto::InputManager::GetLeftThumbStickXY();
+            PR_INFO("Controller 1\nX: {}\nY: {}", vals.X, vals.Y);
+        }
+    }
 };
 
 Presto::Application* Presto::CreateApplication() { return new DualityApp(); }
