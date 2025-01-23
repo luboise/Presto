9d9774af6a20eb4aadbffdcf7d2ece5b31ec2743
luboise <lucas2@live.com.au>
2025-01-23 11:47:35 +1100

    Removed unused random function

--- engine/src/Rendering/Utils/RenderingUtils.cpp
+++ engine/src/Rendering/Utils/RenderingUtils.cpp
@@ -1,16 +1,6 @@
 #include "RenderingUtils.h"
 
 namespace Presto {
-
-    constexpr int COLOUR_RATIO = 101;
-
-    glm::vec3 RenderingUtils::getRandomRed(const double maxNonRed) {
-        auto vec =
-            glm::vec3(1.0F, ((rand() % COLOUR_RATIO) / 100.0) * maxNonRed,
-                      ((rand() % COLOUR_RATIO) / 100.0) * maxNonRed);
-        return vec;
-    }
-
     glm::mat4 RenderingUtils::getProjectionMatrix(glm::float32 fovDeg,
                                                   uint16_t width,
                                                   uint16_t height,
--- engine/src/Rendering/Utils/RenderingUtils.h
+++ engine/src/Rendering/Utils/RenderingUtils.h
@@ -1,8 +1,4 @@
-
 namespace Presto::RenderingUtils {
-
-    glm::vec3 getRandomRed(double);
-
     glm::mat4 getProjectionMatrix(glm::float32 fovDeg, uint16_t width,
                                   uint16_t height, glm::float32 near = 0.01F,
                                   glm::float32 far = 300.0F);
