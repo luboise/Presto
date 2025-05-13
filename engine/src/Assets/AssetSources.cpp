6a066dbbdce85ec277d2b5a5b5493a667ca53082
luboise <lucas2@live.com.au>
2025-05-13 16:42:46 +1000

    Fixed typo

--- engine/src/Assets/AssetSources.cpp
+++ engine/src/Assets/AssetSources.cpp
@@ -86,7 +86,7 @@ void MeshSource::reloadFile() {
         const ImagePtr& image_ptr{
             am.newAsset<ImageAsset>(texture.name, texture.image)};
 
-        textures_[0] = rm.createTexture2D(image_ptr);
+        textures_[i] = rm.createTexture2D(image_ptr);
     }
 
     // Store the material imports
